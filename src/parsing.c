a62fe1a9dad65223a6630578e3b6a53691ede963
Dmitri9149 <dmitri.as.name@gmail.com>
2024-06-29 13:54:30 +0300

    after indent

--- src/parsing.c
+++ src/parsing.c
@@ -9,43 +9,46 @@
 
 static char buffer[2028];
 
-char* readline(char* prompt) {
-   fputs(prompt, stdout);
-   fgets(buffer, 2048, stdin);
-   char* cpy = malloc(strlen(buffer)+1);
-   strcpy(cpy, buffer);
-   cpy[strlen(cpy)-1]='\0';
-   return cpy
-}
+char *readline(char *prompt)
+{
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char *cpy = malloc(strlen(buffer) + 1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy) - 1] = '\0';
+return cpy}
 
-void add_history(char* unused) {}
+void add_history(char *unused)
+{
+}
 
 /* Otherwise include the editline headers */
 #else
 #include <editline/readline.h>
-#include <editline/history.h> 
+#include <editline/history.h>
 #endif
 
-int main(int argc , char** argv) {
-  /* Print Version and Exit Information */
-  puts("Lispy version 0.0.0.0.1");
-  puts("Press Ctrl+c to Exit\n ");
+int main(int argc, char **argv)
+{
+    /* Print Version and Exit Information */
+    puts("Lispy version 0.0.0.0.1");
+    puts("Press Ctrl+c to Exit\n ");
 
- /* In a never ending loop */
- while (1) {
-    /* Output our prompt and get input*/
-    char* input = readline("lispy> ");
+    /* In a never ending loop */
+    while (1) {
+        /* Output our prompt and get input */
+        char *input = readline("lispy> ");
 
-    /* Add input to history */
-    add_history(input);
+        /* Add input to history */
+        add_history(input);
 
-    /* Echo input back to user */
-    printf("No you are a %s\n", input );
+        /* Echo input back to user */
+        printf("No you are a %s\n", input);
 
-    /* Free retrieved input */
-    free(input);
- }
+        /* Free retrieved input */
+        free(input);
+    }
 
- return 0;
- 
+    return 0;
+
 }
