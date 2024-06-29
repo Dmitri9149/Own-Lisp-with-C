6b0cab3855231f876dfb6bc67ba5547cbf23b17c
Dmitri9149 <dmitri.as.name@gmail.com>
2024-06-29 15:16:09 +0300

    parsing user input

--- src/parsing.c
+++ src/parsing.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 
 #include "mpc.h"
 
@@ -16,7 +16,7 @@ char *readline(char *prompt)
     char *cpy = malloc(strlen(buffer) + 1);
     strcpy(cpy, buffer);
     cpy[strlen(cpy) - 1] = '\0';
-return cpy}
+    return cpy}
 
 void add_history(char *unused)
 {
@@ -28,12 +28,12 @@ void add_history(char *unused)
 #include <editline/history.h>
 #endif
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     /* Create Some Parsers */
     mpc_parser_t* Number = mpc_new("number");
     mpc_parser_t* Operator = mpc_new("operator");
-    mpc_parser_t* Expr = mpc_new("expression");
+    mpc_parser_t* Expr = mpc_new("expr");
     mpc_parser_t* Lispy    = mpc_new("lispy");
 
     /* Define them with the following Language */
@@ -59,8 +59,17 @@ int main(int argc, char **argv)
         /* Add input to history */
         add_history(input);
 
-        /* Echo input back to user */
-        printf("No you are a %s\n", input);
+        /* Attempt to parse the user input */
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Lispy, &r)) {
+        /* On success print and delete the AST */
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+        /* Otherwise print and delete the Error */
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
 
         /* Free retrieved input */
         free(input);
@@ -68,7 +77,7 @@ int main(int argc, char **argv)
 
     /* Undefine and Delete our Parsers */
     mpc_cleanup(4, Number, Operator, Expr, Lispy);
-    
+
     return 0;
 
 }
